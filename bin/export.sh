194359b79990c37531fef376b86291baa19ccc12
Damian Harouff <damian@synergycp.com>
2026-01-20 14:37:03 -0700

    Improved export script exclusions and output

--- bin/export.sh
+++ bin/export.sh
@@ -4,4 +4,6 @@ bin/docker-exec rm -rf vendor Scp/vendor || exit $?
 bin/docker-exec composer install --no-dev || exit $?
 bin/docker-exec bash -c "cd Scp; composer install --no-dev" || exit $?
 OUTFILE=/scp/install.synergycp.com/bm/integration/whmcs/synergycp.zip
-exec zip -r "$OUTFILE" . -x ".git*" ".idea/*" "bin/*" ".composer/*"
+rm -f "$OUTFILE"
+zip -r "$OUTFILE" . -x "*.git*" "*.idea/*" "bin/*" ".composer/*" "*CLAUDE.md" "*.claude/*" || exit $?
+echo "Output: $OUTFILE"
